36fbda05bf019ae3d7d1d364bbb1541c7e7b853d
Parichay Dutta Biswas <69819505+Parichay1811@users.noreply.github.com>
2024-12-27 12:00:10 +0530

    changes

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 const newDiv = document.createElement("div");
 
-        // Set the style properties for the div
         newDiv.style.width = "300px";
         newDiv.style.height = "150px";
         newDiv.style.backgroundColor = "yellow";
-
-        // Create a new text node and append it to the div
         const newContent = document.createTextNode("Hello, I'm a div!");
         newDiv.appendChild(newContent);
-
-        // Style the text inside the div
         newDiv.style.color = "blue";
         newDiv.style.fontWeight = "bold";
         newDiv.style.padding = "20px";
-
-        // Append the div to the body
         document.body.appendChild(newDiv);
